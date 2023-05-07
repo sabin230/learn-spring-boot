0b80bf8a14a9bf7af62ecdff6fb86e1d5cb5c161
sabin230 <sabinpradhan@gmail.com>
2023-05-07 10:53:42 -0400

    initial commit
    initial commit

---
+++ src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,19 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourse(){
+        return Arrays.asList( new Course(1,"Learn AWS","in28minutes"),
+                new Course(2,"Learn DevOps","in28minutes"),
+                new Course(3,"Learn devtools","in28minutes"),
+                new Course(4,"Learn Oracle","in28minutes")
+        );
+    }
+}
